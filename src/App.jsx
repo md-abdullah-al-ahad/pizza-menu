3893543e2ce4c4c8a7a0e99f7b98b7b68c983cc0
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-09-19 10:31:04 +0600

    Add mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -108,12 +108,12 @@ function Footer() {
 function Pizza({ pizzaObj }) {
   // if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "Sold Out" : pizzaObj.price}</span>
       </div>
     </li>
   );
